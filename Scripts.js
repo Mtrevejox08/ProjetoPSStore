1e7d71eef77719ccd36f151f8c77206adefd2c37
Mtrevejox08 <trevejomateus@gmail.com>
2023-05-24 07:59:44 -0300

    Projeto-PS-Store

---
+++ Scripts.js
@@ -0,0 +1,16 @@
+const menuList = document.getElementById("menu-list")
+const menuIcon = document.getElementsByClassName("menu-icon")
+
+menuList.style.maxHeight = "0px"
+
+const showMenu = () => {
+    if (menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "120px"
+    }
+    else {
+        menuList.style.maxHeight = "0px"
+    }
+}
+
+
+
